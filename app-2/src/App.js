fcbf0661be834966de74480d8bb8248c1b063d53
Austin Chabaud <austinchabaud@Austins-MacBook-Pro.local>
2019-08-14 15:01:36 -0700

    react drills

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -3,18 +3,20 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      citys: ['Chicago', 'Los Angeles', 'New York', 'Houston', 'Phoenix']
+    };
+  }
+
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    let statesToDisplay = this.state.citys.map((element, index) => {
+      return <h2 key={index}>{element}</h2>;
+    })
+
+    return <div className="App">{statesToDisplay}</div>
   }
 }
 
